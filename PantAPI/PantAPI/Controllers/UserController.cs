8355880ebf688260e65f882c5f340b2234d6da28
Martin Gravråk <martin.gravrak@bouvet.no>
2019-02-01 21:52:47 +0100

    Get userid

---
+++ PantAPI/PantAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController: ControllerBase
+    {
+        [HttpGet]
+        [Route("Register")]
+        [ProducesResponseType(typeof(string), 200)]
+        public ActionResult Register()
+        {
+            string id = Guid.NewGuid().ToString();
+            return Ok(id);
+        }
+
+
+    }
+}
